5e3e73411f04302ac9cd1e69b5f95724577b79a7
Ray <105521700+Barrow75@users.noreply.github.com>
2023-08-14 02:19:28 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,98 @@
+import sys
+import matplotlib
+import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
+
+nnfs.init()
+'''
+inputs = [1, 2, 3, 2.5]
+weights = [[0.2, 0.8, -0.5, 1.0],
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
+biases = [2, 3, 0.5]
+
+output = np.dot(weights, inputs) + biases
+print(output)
+'''
+'''
+np.random.seed(0)
+X = [[1, 2, 3, 2.5],
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]]
+
+X, y = spiral_data(100, 3)
+
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+
+layer1 = Layer_Dense(2, 5)
+activation1 = Activation_ReLU()
+# layer2 = Layer_Dense(5, 2)
+
+layer1.forward(X)
+activation1.forward(layer1.output)
+print(activation1.output)
+# layer2.forward(layer1.output)
+# print(layer2.output)
+'''
+
+layer_outputs = [[4.8, 1.21, 2.385],
+                 [8.9, -1.81, 0.2],
+                 [1.41, 1.051, 0.026]]
+
+exp_values = np.exp(layer_outputs)
+# print(np.sum(layer_outputs, axis=1, keepdims=True))
+
+norm_values = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+print(norm_values)
+
+
+# print(sum(norm_values))
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
+
+X, y = spiral_data(samples=100, classes=3)
+
+dense1 = Layer_Dense(2, 3)
+activation1 = Activation_ReLU()
+
+dense2 = Layer_Dense(3, 3)
+activation2 = Activation_Softmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward((dense2.output))
+print(activation2.output[:5])
